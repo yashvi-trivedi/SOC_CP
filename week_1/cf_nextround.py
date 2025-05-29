c7a5d1bea71ce455a09d1b1d3a94beca4e60a669
Yashvi Trivedi <taezen3.14@gmail.com>
2025-05-30 02:24:33 +0530

    Add Week 1 solutions

--- week_1/cf_nextround.py
+++ week_1/cf_nextround.py
@@ -5,7 +5,7 @@ i=0
 str_arr=input().split()
 ans=0
 for i in range(n):
-    if int(str_arr[i])>=int(str_arr[k-1]) and int(str_arr[k-1])!=0:
+    if int(str_arr[i])>=int(str_arr[k-1]) and int(str_arr[i])!=0:
         ans+=1
 print(ans)
 
